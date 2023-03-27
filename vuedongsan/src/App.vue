c4308551e2a3e39f1f092f24deb511d82c1c4812
jjenniyun <hjkjj1847@gmail.com>
2023-03-27 13:49:01 +0900

    vue_version버전변경(23.03.27)

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    안녕하세요
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
