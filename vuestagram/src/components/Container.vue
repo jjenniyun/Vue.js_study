6bb660889984610e1b2e4d5b66cfee638b8f6cf6
jjenniyun <hjkjj1847@gmail.com>
2023-03-29 14:46:07 +0900

    vue_setting(23.03.29)

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Post :post="post[i]" v-for="(a, i) in post" :key="i" />
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    components: {
+        Post,
+    },
+    props: {
+        post: Array,
+    },
+
+}
+</script>
+
+<style></style>
